3074aae1c3aff1de682fe5d3f0c940410472556d
valeria-io <cortezvacadiez@gmail.com>
2019-04-21 22:15:48 +0200

    added documentation

--- data_cleaning/pre_process.py
+++ data_cleaning/pre_process.py
@@ -2,10 +2,16 @@ import pandas as pd
 
 
 def pre_process_raw_data(raw_data_df):
+    """
+    Formats the column types and filters loans to EE only
+    :param raw_data_df: raw loan data from Bondora
+    :return: original data but filtered for EE loans in the right column type format
+    """
 
     df = raw_data_df.copy()
 
-    # datetime
+    df = df[(df["Country"] == "EE")]
+
     df["ListedOnUTC"] = pd.to_datetime(df['ListedOnUTC'])
     df["LoanDate"] = pd.to_datetime(df['LoanDate'])
     df["MaturityDate_Original"] = pd.to_datetime(df['MaturityDate_Original'])
@@ -13,10 +19,8 @@ def pre_process_raw_data(raw_data_df):
     df["DateOfBirth"] = pd.to_datetime(df['DateOfBirth'])
     df["DefaultDate"] = pd.to_datetime(df['DefaultDate'])
 
-    # numeric
     df["LoanDuration"] = pd.to_numeric(df["LoanDuration"])
 
-    # object
     df['CreditScoreEeMini'] = df['CreditScoreEeMini'].astype(object)
     df['isLate'] = df['isLate'].astype(object)
     df['Defaulted'] = df['Defaulted'].astype(object)
