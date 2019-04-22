081f70f7226ac35033a12c8e0807dad819f7e425
valeria-io <cortezvacadiez@gmail.com>
2019-04-22 15:40:35 +0100

    added new columns and types for column formats

--- data_cleaning/pre_process.py
+++ data_cleaning/pre_process.py
@@ -1,11 +1,12 @@
 import pandas as pd
+import numpy as np
 
 
 def pre_process_raw_data(raw_data_df):
     """
     Formats the column types and filters loans to EE only
     :param raw_data_df: raw loan data from Bondora
-    :return: original data but filtered for EE loans in the right column type format
+    :return: original data with EE loans only and in the right column type format
     """
 
     df = raw_data_df.copy()
@@ -21,12 +22,17 @@ def pre_process_raw_data(raw_data_df):
 
     df["LoanDuration"] = pd.to_numeric(df["LoanDuration"])
 
-    df['CreditScoreEeMini'] = df['CreditScoreEeMini'].astype(object)
-    df['isLate'] = df['isLate'].astype(object)
-    df['Defaulted'] = df['Defaulted'].astype(object)
-    df['NrOfDependants'] = df['NrOfDependants'].astype(object)
+    df['CreditScoreEeMini'] = df['CreditScoreEeMini'].astype(str)
+    df['isLate'] = df['isLate'].astype(str)
+    df['Defaulted'] = df['Defaulted'].astype(str)
+    df['NrOfDependants'] = df['NrOfDependants'].astype(str)
+
+    df["Default Status"] = df["Defaulted"].apply(
+        lambda x: "Non defaulter" if x == 0 else "Defaulter")
+    df["Age"] = df["Age"].apply(lambda x: "Under 40" if x <= 40 else "Over 40")
+
+    df = df.replace(-1, np.nan)
 
     return df
 
-def fill_na_with_k_nearest_neighbours():
-    return -1
+
