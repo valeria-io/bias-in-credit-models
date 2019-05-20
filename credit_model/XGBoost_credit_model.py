c2d9fdd1ec0464e951aeda48bda5efc82268fc75
valeria-io <cortezvacadiez@gmail.com>
2019-05-20 21:04:22 +0100

    runs xgboost with best params

--- credit_model/XGBoost_credit_model.py
+++ credit_model/XGBoost_credit_model.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from xgboost.sklearn import XGBClassifier
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_auc_score
 
 from credit_model.prepare_model_dataframe import prepare_dataframe
 
@@ -12,21 +12,17 @@ def find_best_xgboost_model(train_x, train_y):
     scale_pos_weight = (len(train_y) - train_y.sum()) / train_y.sum()
 
     param_test = {
-        'max_depth': range(3, 11),
-        'gamma': [i / 10.0 for i in range(0, 5)],
-        'min_child_weight': range(1, 15, 1),
-        'subsample': [i / 10.0 for i in range(6, 10)],
-        'colsample_bytree': [i / 10.0 for i in range(6, 10)],
-        'reg_alpha': [1e-5, 1e-2, 0.1, 1, 100]
+        'max_depth': [8, 9, 10, 11],
+        'learning_rate': [0.015, 0.05, 0.1, 0.15],
+        'n_estimators': [100, 150, 200, 300, 400, 500]
     }
 
     gsearch = GridSearchCV(estimator=XGBClassifier(
-        learning_rate=0.01,
         objective='binary:logistic',
         nthread=4,
         scale_pos_weight=scale_pos_weight,
         seed=27),
-        param_grid=param_test, scoring='roc_auc', n_jobs=4, iid=False, cv=10)
+        param_grid=param_test, scoring='roc_auc', n_jobs=-1, iid=False, cv=5)
 
     gsearch.fit(train_x, train_y)
 
@@ -36,23 +32,18 @@ def find_best_xgboost_model(train_x, train_y):
 def xgboost_predict(best_params_, train_x, train_y, test_x, test_y):
     scale_pos_weight = (len(train_y) - train_y.sum()) / train_y.sum()
     xgb_model = XGBClassifier(objective='binary:logistic',
-                              learning_rate=0.01,
-                              n_jobs=4,
                               scale_pos_weight=scale_pos_weight,
                               seed=27,
                               max_depth=best_params_['max_depth'],
-                              gamma=best_params_['gamma'],
-                              min_child_weight=best_params_['min_child_weight'],
-                              subsample=best_params_['subsample'],
-                              colsample_bytree=best_params_['colsample_bytree'],
-                              reg_alpha=best_params_['reg_alpha'])
+                              learning_rate=best_params_['learning_rate'],
+                              n_estimators=best_params_['n_estimators']
+                              )
 
     xgb_model.fit(train_x, train_y)
+    predicted_probabilities_ = xgb_model.predict_proba(test_x)[:, 1]
+    auc_ = roc_auc_score(test_y, predicted_probabilities_)
 
-    predictions_ = xgb_model.predict(test_x)
-    accuracy_ = accuracy_score(test_y, predictions)
-
-    return predictions_, accuracy_
+    return predicted_probabilities_, auc_
 
 
 def get_train_test_dataframes(df_, target_variable):
@@ -68,6 +59,6 @@ if __name__ == "__main__":
     train_X, test_X, train_Y, test_Y = get_train_test_dataframes(df, "Defaulted")
     best_params, best_score = find_best_xgboost_model(train_X, train_Y)
     print('Best Parameters: {} | Best AUC: {}'.format(best_params, best_score))
-    predictions, accuracy = xgboost_predict(best_params, train_X, train_Y, test_X, test_Y)
-    print("Accuracy: {}.2f%%".format(accuracy * 100.0))
-    pd.DataFrame({'actuals': test_Y, 'predictions': predictions}).to_csv('../data/actuals_and_forecasts.csv')
+    predicted_probabilities, auc = xgboost_predict(best_params, train_X, train_Y, test_X, test_Y)
+    print("AUC: {}".format(auc))
+    pd.DataFrame({'actuals': test_Y, 'predicted_probabilities': predicted_probabilities}).to_csv('../data/actuals_and_forecasts.csv')
